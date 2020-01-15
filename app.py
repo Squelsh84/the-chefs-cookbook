16f853a909ffb29d2ad2e6e1d1b78d644edbb3c6
squelsh84 <mrmjwalsh@gmail.com>
2020-01-15 00:44:27 +0000

    Testing recipes.html to see if there is a connection with the mongodb

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["MONGO_DBNAME"] = os.getenv("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template('recipes.html', recipes=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+            port=(os.environ.get('PORT')),
             debug=True)
